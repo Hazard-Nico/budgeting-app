5be2398daea3f00e3a3309f7e232dd011b232935
Hazard-Nico <nugrahaji98@gmail.com>
2026-03-16 22:43:59 +0700

    to add ignore eslint

--- frontend/okane-frontend/next.config.mjs
+++ frontend/okane-frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors. Use this option with caution.
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
